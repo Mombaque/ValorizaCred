636b04889939e9c8bcb2af3aeb349d5476943a38
Diogo Mombaque <diogo.mombaque@neobpo.com.br>
2022-07-31 15:17:50 -0300

    Updates

---
+++ precache-manifest.b5eebe84d44b34965a4b5c73995dba5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3e3780aeb178c195808a6c415aae63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b8c9025acb641c1ac25",
+    "url": "/static/css/2.4f4edfad.chunk.css"
+  },
+  {
+    "revision": "1aa1a5fb708bd833694c",
+    "url": "/static/css/main.fda4f1f4.chunk.css"
+  },
+  {
+    "revision": "3b8c9025acb641c1ac25",
+    "url": "/static/js/2.7f1810d2.chunk.js"
+  },
+  {
+    "revision": "60ecaad85320638366e6b61932d38edb",
+    "url": "/static/js/2.7f1810d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa1a5fb708bd833694c",
+    "url": "/static/js/main.6c347fdd.chunk.js"
+  },
+  {
+    "revision": "6fd4d31c9213f7e00975",
+    "url": "/static/js/runtime-main.c9601250.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ValorizaCred/precache-manifest.88ebf6d00caa7bc180c72ac93944b904.js"
+  "/precache-manifest.b5eebe84d44b34965a4b5c73995dba5b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ValorizaCred/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
