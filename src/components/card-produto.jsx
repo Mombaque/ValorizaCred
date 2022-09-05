7bdff24ce75b30c85d3bb87fa97e7c202c5157c7
Diogo Mombaque <diogo.mombaque@neobpo.com.br>
2022-09-05 17:53:03 -0300

    Ajuste nas imagens (mobile)

--- src/components/card-produto.jsx
+++ src/components/card-produto.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import {WHATSAPP_URL} from '../App'
 import '../css/card-produto.css'
