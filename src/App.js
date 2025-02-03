d9d8f959911012cd9cc998eac996f1e3fceb2533
Diogo <borgesdiogo92@gmail.com>
2025-02-02 23:16:16 -0300

    wip

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./css/card.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const WHATSAPP_URL = "https://wa.me/5515998256777?text=Olá,%20gostaria%20de%20mais%20algumas%20informações";
+export const WHATSAPP_URL = "https://wa.me/5515998256777?text=Oi,%20encontrei%20o%20site%20da%20sua%20empresa%20e%20gostaria%20de%20esclarecer%20algumas%20dúvidas.%20Pode%20me%20ajudar?";
 
 const App = () => {
     // const TRACKING_ID = "UA-203501474-1";
--- src/components/models/products.jsx
+++ src/components/models/products.jsx
@@ -6,20 +6,21 @@ import DescriptionGarantiaVeiculo from "../product-description/description-garan
 export const PRODUTOS = [
     {
         id: 1,
+        image: 'principal.jpg',
+    },
+    {
+        id: 2,
         image: 'inss-1200x1200.png',
         isRow: true, 
         description: <DescriptionInss />
     },
     {
-        id: 2,
+        id: 3,
         image: 'credito-garantia-imovel.png', 
-        //description: <DescriptionGarantiaImovel url="https://loja.franq.com.br/pb/heleide-borges/emprestimos/55"/>
     },
     {
-        id: 3,
+        id: 4,
         image: 'credito-garantia-veiculo.jpeg', 
-        //description: <DescriptionGarantiaVeiculo url="https://loja.franq.com.br/pb/heleide-borges/emprestimos/120"/>
     },
-    // {image: 'emprestimo.jpeg'},
     {image: 'fgts.jpeg'}, 
 ];
